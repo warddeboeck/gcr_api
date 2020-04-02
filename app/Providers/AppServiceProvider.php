558fd51ff1808f6457893aa09cd82904ec6190e3
Ward De Boeck <ward@supermachine.be>
2020-04-02 21:06:40 +0200

    from to

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Mailbox::from('{idea_code}@idea.globalcreativereview.com', function (InboundEmail $email, $idea_code) {
+        Mailbox::to('{idea_code}@idea.globalcreativereview.com', function (InboundEmail $email, $idea_code) {
             // $sender = $email->from();
             $user = User::where('email', 'w_de_boeck@hotmail.com')->first();
             $user->idea_url = 'test';
