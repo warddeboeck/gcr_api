7219de807af8f317a6405ce6840caf0da01e015b
Ward De Boeck <ward@supermachine.be>
2020-04-03 20:33:37 +0200

    schedule every five minutes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('users:makematches')->withoutOverlapping();
+        $schedule->command('users:makematches')->everyFiveMinutes()->withoutOverlapping();
     }
 
     /**
